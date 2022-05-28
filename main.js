ef64b05c24b208bd7c3b83eda43a843265cb8486
mahmoudalghareeb253@gmail.com <tato.alghareeb@gmail.com>
2022-05-29 01:51:38 +0200

    educational site

---
+++ main.js
@@ -0,0 +1,75 @@
+let bars = document.querySelector("#menu");
+let navbar = document.querySelector(".navbar");
+
+bars.addEventListener("click", function () {
+  this.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+  bars.classList.remove("fa-times");
+  navbar.classList.remove("active");
+});
+
+let swiper = new Swiper(".course-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+swiper = new Swiper(".teachers-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
